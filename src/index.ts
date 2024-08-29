ea2e3c2bed7701c3def9a3ee0a249c8211cb8256
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2024-08-29 15:40:00 +0200

    Add a DO that throws errors

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { DurableObject } from "cloudflare:workers";
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+  MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>;
+}
+
+export class MyDurableObject extends DurableObject {
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+  }
+
+  async throwObject(): Promise<string> {
+    const data = { data: "IMPORTANT DATA GOES HERE" };
+    throw new Error(JSON.stringify(data));
+  }
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+  async fetch(request, env, ctx): Promise<Response> {
+    const id: DurableObjectId = env.MY_DURABLE_OBJECT.idFromName(
+      new URL(request.url).pathname
+    );
+
+    const stub = env.MY_DURABLE_OBJECT.get(id);
+    let output = "";
+    try {
+      output = await stub.throwObject();
+    } catch (e: any) {
+      return new Response(e.message);
+    }
+    return new Response(output);
+  },
+} satisfies ExportedHandler<Env>;
